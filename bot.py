d40a63ba192e57464a5f5a3606000b06a31602b8
parthchilwerwar <chilwerwarparth23@gmail.com>
2021-07-10 12:19:39 +0530

    new weather cast command

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
 from discord.ext.commands import CommandNotFound
+import requests
 import datetime
+import json
 import random
 import asyncio
 from pyrandmeme import *
@@ -13,6 +15,8 @@ from io import BytesIO
 
 client = commands.Bot(command_prefix='k~')
 client.remove_command('help')
+api_key = "6beb4cf35bb9797f16db57512aaa1307"
+base_url = "http://api.openweathermap.org/data/2.5/weather?"
 
 
 @client.event
@@ -561,7 +565,34 @@ async def truth(ctx):
     await ctx.send(embed = embed)
 
 
-
+@client.command()
+async def weather(ctx, *, city: str):
+    city_name = city
+    complete_url = base_url + "appid=" + api_key + "&q=" + city_name
+    response = requests.get(complete_url)
+    x = response.json()
+    channel = ctx.message.channel
+    if x["cod"] != "404":
+        async with channel.typing():
+            y = x["main"]
+            current_temperature = y["temp"]
+            current_temperature_celsiuis: str = str(round(current_temperature - 273.15))
+            current_pressure = y["pressure"]
+            current_humidity = y["humidity"]
+            z = x["weather"]
+            weather_description = z[0]["description"]
+            embed = discord.Embed(title=f"Weather in {city_name}",
+                                  color=ctx.guild.me.top_role.color,
+                                  timestamp=ctx.message.created_at, )
+            embed.add_field(name="Descripition", value=f"**{weather_description}**", inline=False)
+            embed.add_field(name="Temperature(C)", value=f"**{current_temperature_celsiuis}°C**", inline=False)
+            embed.add_field(name="Humidity(%)", value=f"**{current_humidity}%**", inline=False)
+            embed.add_field(name="Atmospheric Pressure(hPa)", value=f"**{current_pressure}hPa**", inline=False)
+            embed.set_thumbnail(url="https://i.ibb.co/CMrsxdX/weather.png")
+            embed.set_footer(text=f"Requested by {ctx.author.name}")
+            await channel.send(embed=embed)
+    elif not x["cod"] != "404":
+        await channel.send("City not found.")
 
 
 

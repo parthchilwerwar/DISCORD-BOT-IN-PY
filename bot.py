78f91519c024cba477b7340b0a2f7b145879744d
parthchilwerwar <chilwerwarparth23@gmail.com>
2022-01-26 11:41:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ base_url = "http://api.openweathermap.org/data/2.5/weather?"
 @client.event
 async def on_ready():
 
-	await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"THAT YOU WILL USE | kan  FOR HELP "))
+	await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"FOR HELP USE `kan` "))
 
 	print("Kan is online")
 	
